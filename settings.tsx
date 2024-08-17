ac9ba962d1ea627f2fa799116533064f7876df31
ohim29 <129508177+ohim29@users.noreply.github.com>
2024-08-17 21:51:16 +0200

    Add files via upload

--- settings.tsx
+++ settings.tsx
@@ -1,12 +1,12 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "Public", mintText: "Mint Paying with DEF", buttonLabel: "Mint now!", header: "Public Mint" },
+  { label: "Public", mintText: "Mint Paying with 0.3 SOL", buttonLabel: "Mint now!", header: "Public Mint" },
   { label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "Default"}
 ];
 
 //header image in the ui. replace with your own
-export const image = "https://avatars.githubusercontent.com/u/93528482?v=4";
+export const image = "https://arweave.net/TzanTNic2nACr8Wnh0dp9ZGvZgc3zlBDc1SNcjHhdAc?ext=png";
 
 //website title
-export const headerText = "MarkSackerberg's mint UI";
+export const headerText = "SolFrog minting";
