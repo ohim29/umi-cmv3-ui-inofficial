48f35c4716fb3df0fec5bcb200ff7d27f7fd8923
ohim29 <129508177+ohim29@users.noreply.github.com>
2024-08-22 19:48:04 +0200

    Update settings.tsx

--- settings.tsx
+++ settings.tsx
@@ -1,12 +1,12 @@
 // Purpose: Contains texts that will be used in the UI when rendering the buttons
 // label has to be the exact same as in your candy machine config
 export const mintText = [
-  { label: "Public", mintText: "Mint Paying with 0.3 SOL", buttonLabel: "Mint now!", header: "Public Mint" },
+  { label: "Public", mintText: "Mint Paying with 0.1 SOL", buttonLabel: "Mint now!", header: "Public Mint" },
   { label: "default", mintText: "default mintText", buttonLabel: "Mint now!", header: "Default"}
 ];
 
 //header image in the ui. replace with your own
-export const image = "https://arweave.net/TzanTNic2nACr8Wnh0dp9ZGvZgc3zlBDc1SNcjHhdAc?ext=png";
+export const image = "https://arweave.net/TY9HutBdUN3NJ_4mG3IWw5EGeWacd8bREy2Y1NBioq0?ext=png";
 
 //website title
-export const headerText = "SolFrog minting";
+export const headerText = "Solana Frog minting";
